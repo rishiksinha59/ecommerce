42a5d1f5cce845a4937efcd059d9973d00c19613
Rishik Sinha <rishiksinha@Rishiks-MacBook-Air.local>
2024-10-02 19:01:08 +0530

    dn

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 import { ShopContext } from "../context/ShopContext";
 const Navbar = () => {
   const [visible, setVisible] = useState(false);
-  const {showSearch,setShowSearch}=useContext(ShopContext)
+  const {showSearch,setShowSearch, getCartCount}=useContext(ShopContext)
   return (
     <div className="flex items-center justify-between py-5 font-medium">
       <Link to="/">  
@@ -42,7 +42,7 @@ const Navbar = () => {
         </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="" />
-          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-[10px] text-center leading-4 bg-black text-white aspect-square rounded-full">10</p>
+          <p className="absolute right-[-5px] bottom-[-5px] w-4 text-[10px] text-center leading-4 bg-black text-white aspect-square rounded-full">{getCartCount()}</p>
         </Link>
         <img onClick={() => setVisible(true)} src={assets.menu_icon} className="w-5 cursor-pointer sm:hidden" alt="" />
       </div>
--- frontend/src/pages/Product.jsx
+++ frontend/src/pages/Product.jsx
@@ -2,13 +2,14 @@ import React, { useContext, useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { ShopContext } from '../context/ShopContext';
 import { assets } from '../assets/assets';
+import RelatedProducts from '../components/RelatedProducts';
 
 const Product = () => {
   const {productId} = useParams();
-  const {products,currency} = useContext(ShopContext)
+  const {products,currency, addToCart} = useContext(ShopContext)
   const [productData,setProductData] = useState(false);
   const [image,setImage] = useState('');
-
+  const [size, setSize] = useState('');
   const fetchProductData = async()=>{
     products.map((item)=>{
       if(item._id === productId){
@@ -58,12 +59,32 @@ const Product = () => {
                 <p>Select Size</p>
                 <div className='flex gap-2'>
                   {productData.sizes.map((item,index)=>(
-                    <button className={`border py-2 px-4 bg-gray-100`} key={index}>{item}</button>
+                    <button onClick={()=>setSize(item)} className={`border py-2 px-4 bg-gray-100 ${item === size ? 'border-orange-500': ''}`} key={index}>{item}</button>
                   ))}
                 </div>
             </div>
+            <button onClick={()=>addToCart(productData._id,size)} className='bg-black text-white px-8 py-3 text-sm active:bg-gray-700'>ADD TO CART</button>
+            <hr className='mt-8 sm:w-4/5'/>
+            <div className='text-sm text-gray-500 mt-5 '>
+                  <p>100% Original Product</p>
+                  <p>Cash on delivery is available on this product</p>
+                  <p>Easy return and exchange policy within 7 days</p>
+            </div>
         </div>
       </div>
+      {/* ----Description and review section----- */}
+      <div className='mt-20'>
+        <div className='flex'>
+          <b className='border px-5 py-3 text-sm'>Description</b>
+          <p className='border px-5 py-3 text-sm'>Reviews (122)</p>
+        </div>
+        <div className='flex flex-col gap-4 border px-6 py-6 text-sm text-gray-500'>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores quos libero numquam veniam autem vel aliquid, tempora beatae? Sint commodi voluptas ex id reiciendis? Quasi iure sed laudantium ipsum ratione?</p>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae quam explicabo aliquid voluptate! Provident eius, corporis hic nobis quae labore consectetur, odio soluta, optio temporibus deserunt nostrum cupiditate voluptate deleniti.</p>
+        </div>
+      </div>
+      {/* ........display related product....... */}
+      <RelatedProducts category={productData.category} subCategory={productData.subCategory}/>
     </div>
   ) : <div className='opacity-0'></div>
 }
